18ec08cf8fb7d1f2428742133a469fcd24c96612
NicolasIannello <nicolas.iannello99@gmail.com>
2026-06-09 16:12:53 -0300

    WorldComponent, ICommunicable, ILoadReferenceable

--- IWantThis/WorldComponent_IWantThis.cs
+++ IWantThis/WorldComponent_IWantThis.cs
@@ -1,70 +1,38 @@
 ﻿using HarmonyLib;
 using IWantThis.UI;
 using RimWorld;
+using RimWorld.Planet;
 using System.Collections.Generic;
+using System.Linq;
 using Verse;
 
 namespace IWantThis
 {
-    [HarmonyPatch(typeof(Building_CommsConsole), nameof(Building_CommsConsole.GetFloatMenuOptions))]
-    public static class Patch_Building_CommsConsole_GetFloatMenuOptions
+    [HarmonyPatch]
+    public class WorldComponent_IWantThis : WorldComponent, ICommunicable, ILoadReferenceable
     {
-        [HarmonyPostfix]
-        public static void Postfix(Building_CommsConsole __instance, Pawn myPawn, ref IEnumerable<FloatMenuOption> __result)
+        public static WorldComponent_IWantThis Instance;
+        public WorldComponent_IWantThis(World world) : base(world) => Instance = this;
+
+        public string GetCallLabel() => null;
+        public string GetInfoText() => null;
+        public Faction GetFaction() => null;
+        public string GetUniqueLoadID() => world.GetUniqueLoadID() + "_IWantThis";
+
+        public void TryOpenComms(Pawn negotiator)
         {
-            if (__instance.GetComp<CompPowerTrader>().PowerOn)
-            {
-                List<FloatMenuOption> optionsList = new List<FloatMenuOption>(__result);
-
-                void action()
-                {
-                    Find.WindowStack.Add(new Dialog_IWantThis(__instance.MapHeld));
-                    //Messages.Message("status "+__instance.GetComp<CompPowerTrader>().PowerOn, MessageTypeDefOf.PositiveEvent);
-                }
-
-                FloatMenuOption customOption = new FloatMenuOption("IWantThis.PlaceBounty".Translate(), action, MenuOptionPriority.InitiateSocial);
-
-                optionsList.Add(customOption);
-
-                __result = optionsList;
-            }
+            Find.WindowStack.Add(new Dialog_IWantThis(negotiator.MapHeld));
         }
+
+        public FloatMenuOption CommFloatMenuOption(Building_CommsConsole console, Pawn negotiator) =>
+                FloatMenuUtility.DecoratePrioritizedTask(new FloatMenuOption(
+                    "IWantThis.PlaceBounty".Translate(), 
+                    delegate { console.GiveUseCommsJob(negotiator, this); },
+                    MenuOptionPriority.InitiateSocial), negotiator, console);
+
+        [HarmonyPatch(typeof(Building_CommsConsole), nameof(Building_CommsConsole.GetCommTargets))]
+        [HarmonyPostfix]
+        public static IEnumerable<ICommunicable> Postfix(IEnumerable<ICommunicable> targets) => targets.Append(Instance);
+
     }
-
-
-
-
-    //[HarmonyPatch]
-    //public class WorldComponent_IWantThis : WorldComponent, ICommunicable, ILoadReferenceable
-    //{
-    //    public static WorldComponent_IWantThis Instance;
-
-    //    public WorldComponent_IWantThis(World world) : base(world) => Instance = this;
-    //    public string GetCallLabel() => null;
-
-    //    public string GetInfoText() => null;
-    //    public Faction GetFaction() => null;
-
-    //    public void TryOpenComms(Pawn negotiator)
-    //    {
-    //        Find.WindowStack.Add(new Dialog_IWantThis(negotiator.MapHeld));
-    //    }
-
-    //    public FloatMenuOption CommFloatMenuOption(Building_CommsConsole console, Pawn negotiator) =>
-    //        FloatMenuUtility.DecoratePrioritizedTask(new FloatMenuOption("IWantThis.PlaceBounty".Translate(), delegate { console.GiveUseCommsJob(negotiator, this); },
-    //             MenuOptionPriority.InitiateSocial), negotiator, console);
-
-    //    public string GetUniqueLoadID() => world.GetUniqueLoadID() + "_IWantThis";
-
-    //    public override void WorldComponentTick()
-    //    {
-    //        base.WorldComponentTick();
-
-    //    }
-
-    //    [HarmonyPatch(typeof(Building_CommsConsole), nameof(Building_CommsConsole.GetCommTargets))]
-    //    [HarmonyPostfix]
-    //    public static IEnumerable<ICommunicable> GetCommTargets_Postfix(IEnumerable<ICommunicable> targets) => targets.Append(Instance);
-
-    //}
 }
