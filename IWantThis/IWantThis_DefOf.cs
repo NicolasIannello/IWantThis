bf1c4c409cdf20e56a164e4048bb82481d079b3f
NicolasIannello <nicolas.iannello99@gmail.com>
2026-06-11 15:26:51 -0300

    bountyquest

---
+++ IWantThis/IWantThis_DefOf.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+
+namespace IWantThis
+{
+
+    [DefOf]
+    public class IWantThis_DefOf
+    {
+        public static QuestScriptDef IWantThis_BountyQuest;
+
+        static IWantThis_DefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(IWantThis_DefOf));
+        }
+    }
+}
