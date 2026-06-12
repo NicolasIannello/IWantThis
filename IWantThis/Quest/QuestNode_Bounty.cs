39f000833e4c64727df71fec9dc9610098b31437
NicolasIannello <nicolas.iannello99@gmail.com>
2026-06-12 18:13:13 -0300

    handle reputation activebounty

---
+++ IWantThis/Quest/QuestNode_Bounty.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld.QuestGen;
+using Verse;
+
+namespace IWantThis.Quest
+{
+    public class QuestNode_BountyStart : QuestNode
+    {
+        [NoTranslate]
+        public SlateRef<string> inSignal;
+
+        protected override void RunInt()
+        {
+            WorldComponent_IWantThis.Instance.ActiveBounty = true;
+            QuestPart_Bounty questPart = new QuestPart_Bounty();
+
+            int questId = QuestGen.quest.id;
+            questPart.inSignal = "Quest"+questId.ToString();
+
+            QuestGen.quest.AddPart(questPart);
+        }
+
+        protected override bool TestRunInt(Slate slate) => true;
+    }
+}
---
+++ IWantThis/Quest/QuestPart_Bounty.cs
@@ -0,0 +1,40 @@
+﻿using RimWorld;
+using Verse;
+
+namespace IWantThis.Quest
+{
+    public class QuestPart_Bounty : QuestPart
+    {
+        public string inSignal;
+
+        public override void Notify_QuestSignalReceived(Signal signal)
+        {
+            base.Notify_QuestSignalReceived(signal);
+            int rep = WorldComponent_IWantThis.Instance.Reputation;
+            if (signal.tag == inSignal+".pickupShipThing.SentSatisfied")
+            {
+                WorldComponent_IWantThis.Instance.ActiveBounty = false;
+                WorldComponent_IWantThis.Instance.Reputation = rep + 1;
+                Messages.Message("IWantThis.ReputationChange".Translate(), MessageTypeDefOf.NeutralEvent);
+            }
+            if (signal.tag == inSignal + ".pickupShipThing.Destroyed")
+            {
+                WorldComponent_IWantThis.Instance.ActiveBounty = false;
+                WorldComponent_IWantThis.Instance.Reputation = rep - 3;
+                Messages.Message("IWantThis.ReputationChange".Translate(), MessageTypeDefOf.NegativeEvent);
+            }
+            if (signal.tag == inSignal + ".pickupShipThing.SentUnsatisfied")
+            {
+                WorldComponent_IWantThis.Instance.ActiveBounty = false;
+                WorldComponent_IWantThis.Instance.Reputation = rep - 2;
+                Messages.Message("IWantThis.ReputationChange".Translate(),MessageTypeDefOf.RejectInput);
+            }
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref inSignal, nameof(inSignal));
+        }
+    }
+}
